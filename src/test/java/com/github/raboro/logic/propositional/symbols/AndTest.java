b39d251a789ca380d0a01bc1cf0754d6cad1225f
Raboro <marius.woerfelreal@gmail.com>
2023-03-31 17:21:23 +0200

    added tests for and

---
+++ src/test/java/com/github/raboro/logic/propositional/symbols/AndTest.java
@@ -0,0 +1,49 @@
+package com.github.raboro.logic.propositional.symbols;
+
+
+import org.junit.jupiter.api.Test;
+
+import static com.github.raboro.logic.propositional.symbols.And.and;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * @author Raboro
+ * @since 1.0-SNAPSHOT
+ */
+class AndTest {
+
+    @Test
+    void testStaticAndTwoInputs() {
+        assertFalse(and(false, false));
+        assertFalse(and(true, false));
+        assertFalse(and(false, true));
+        assertTrue(and(true, true));
+    }
+
+    @Test
+    void testStaticAndMultipleInputs() {
+        assertFalse(and(false, true, false, false));
+        assertFalse(and(false, true, false));
+        assertFalse(and(true, false, true, true, false, true));
+        assertTrue(and(true, true, true, true, true, true));
+        assertTrue(and(true, true, true));
+    }
+
+    @Test
+    void testAndTwoInputs() {
+        assertFalse(new And(false, false).value());
+        assertFalse(new And(true, false).value());
+        assertFalse(new And(false, true).value());
+        assertTrue(new And(true, true).value());
+    }
+
+    @Test
+    void testAndMultipleInputs() {
+        assertFalse(new And(false, true, false, false).value());
+        assertFalse(new And(false, true, false).value());
+        assertFalse(new And(true, false, true, true, false, true).value());
+        assertTrue(new And(true, true, true, true, true, true).value());
+        assertTrue(new And(true, true, true).value());
+    }
+}
