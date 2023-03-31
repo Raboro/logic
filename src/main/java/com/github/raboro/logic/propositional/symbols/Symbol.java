4bfb32ad618e9a996c0d515529a224e8838c3ae4
Raboro <marius.woerfelreal@gmail.com>
2023-03-31 17:20:04 +0200

    added symbol interface

---
+++ src/main/java/com/github/raboro/logic/propositional/symbols/Symbol.java
@@ -0,0 +1,12 @@
+package com.github.raboro.logic.propositional.symbols;
+
+/**
+ * @author Raboro
+ * @since 1.0-SNAPSHOT
+ */
+public interface Symbol {
+
+    boolean value();
+
+    String symbol();
+}
