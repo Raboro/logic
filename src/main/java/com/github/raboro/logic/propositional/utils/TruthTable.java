31b64b7b3e2e553a582c1c2cc8ca988138b32fa9
Raboro <raborogit@gmail.com>
2023-04-02 19:21:38 +0200

    added buildRow implementation

--- src/main/java/com/github/raboro/logic/propositional/utils/TruthTable.java
+++ src/main/java/com/github/raboro/logic/propositional/utils/TruthTable.java
@@ -2,6 +2,8 @@ package com.github.raboro.logic.propositional.utils;
 
 import com.github.raboro.logic.propositional.symbols.Symbol;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -14,11 +16,14 @@ public class TruthTable {
     private final Symbol symbol;
     private String header;
     private String spaceRow;
+    private final List<String> rows;
 
     public TruthTable(Symbol symbol) {
         this.symbol = symbol;
+        rows = new ArrayList<>();
         buildSpaceRow();
         buildHeader();
+        buildRows();
     }
 
     private void buildSpaceRow() {
@@ -29,5 +34,23 @@ public class TruthTable {
         header = IntStream.range(1, symbol.LENGTH + 1)
                 .mapToObj(i -> " " + i + " |")
                 .collect(Collectors.joining("", "|", "| y " + "|"));
+    }
+
+    private void buildRows() {
+        for (int row = 0; row < Math.pow(2, symbol.LENGTH); row++) {
+            final String binaryNumber = createBinary(row, symbol.LENGTH);
+            final String builder = IntStream.range(0, binaryNumber.length())
+                    .mapToObj(i -> " " + binaryNumber.charAt(i) + " |")
+                    .collect(Collectors.joining("", "|", "| " + (symbol.valueEquals(binaryNumber) ? "1" : "0") + " |"));
+            rows.add(builder);
+        }
+    }
+
+    private String createBinary(int decimalNumber, int fillLength) {
+        final StringBuilder binaryNumber = new StringBuilder(Integer.toBinaryString(decimalNumber));
+        while (binaryNumber.length() < fillLength) {
+            binaryNumber.insert(0, "0");
+        }
+        return binaryNumber.toString();
     }
 }
