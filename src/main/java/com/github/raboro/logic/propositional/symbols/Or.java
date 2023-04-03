cd1ad0cb3838b959d5e7b36f9285a9cf8ee40a45
Raboro <marius.woerfelreal@gmail.com>
2023-04-03 22:19:07 +0200

    removed braces

--- src/main/java/com/github/raboro/logic/propositional/symbols/Or.java
+++ src/main/java/com/github/raboro/logic/propositional/symbols/Or.java
@@ -39,9 +39,8 @@ public class Or extends Symbol {
     @Override
     public boolean valueEquals(String reference) {
         final boolean[] referenceValues = new boolean[reference.length()];
-        for (int i = 0; i < reference.length(); i++) {
+        for (int i = 0; i < reference.length(); i++)
             referenceValues[i] = reference.charAt(i) != '0';
-        }
         return or(referenceValues) == value();
     }
 
