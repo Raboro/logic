6c8e591decff3269a61c6c1a492681ac1f3b0a06
Raboro <marius.woerfelreal@gmail.com>
2023-04-07 13:14:26 +0200

    implemented Nand

---
+++ src/main/java/com/github/raboro/logic/propositional/symbols/Nand.java
@@ -0,0 +1,74 @@
+package com.github.raboro.logic.propositional.symbols;
+
+import static com.github.raboro.logic.propositional.symbols.And.and;
+
+/**
+ * @author Raboro
+ * @since 1.0-SNAPSHOT
+ */
+public class Nand extends Symbol {
+
+    private final boolean[] values;
+
+    public Nand(boolean a, boolean b) {
+        this(new boolean[]{a, b});
+    }
+
+    public Nand(boolean... values) {
+        super("\u22BC", values);
+        this.values = values;
+    }
+
+    public static boolean nand(boolean a, boolean b) {
+        return !(a && b);
+    }
+
+    public static boolean nand(boolean... values) {
+        boolean result = values[0];
+        for (int i = 1; i < values.length; i++) {
+            result = and(result, values[i]);
+        }
+        return !result;
+    }
+
+    @Override
+    public boolean value() {
+        boolean result = values[0];
+        for (int i = 1; i < values.length; i++) {
+            result = and(result, values[i]);
+        }
+        return !result;
+    }
+
+    @Override
+    public boolean valueOf(boolean... values) {
+        return new Nand(values).value();
+    }
+
+    @Override
+    public boolean valueOf(String reference) {
+        return new Nand(constructReference(reference)).value();
+    }
+
+    @Override
+    public boolean valueEquals(String reference) {
+        return nand(constructReference(reference)) == value();
+    }
+
+    @Override
+    public boolean valueEquals(boolean... reference) {
+        return reference.length == values.length && nand(reference) == value();
+    }
+
+    public void baseTruthTable() {
+        baseTruthTable(2);
+    }
+
+    public void baseTruthTable(int variableCounter) {
+        super.baseTruthTable(construct(super.constructParameters(variableCounter)));
+    }
+
+    private Symbol construct(boolean... values) {
+        return new Nand(values);
+    }
+}
