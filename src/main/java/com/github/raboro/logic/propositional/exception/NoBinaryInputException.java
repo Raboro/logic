e977e0402021a75b1583ba2245fa544797a7d821
Raboro <marius.woerfelreal@gmail.com>
2023-04-08 17:23:57 +0200

    added Javadoc to exceptions

--- src/main/java/com/github/raboro/logic/propositional/exception/NoBinaryInputException.java
+++ src/main/java/com/github/raboro/logic/propositional/exception/NoBinaryInputException.java
@@ -1,5 +1,9 @@
 package com.github.raboro.logic.propositional.exception;
 
+/**
+ * @author Raboro
+ * @since 1.0-SNAPSHOT
+ */
 public class NoBinaryInputException extends RuntimeException {
 
     public NoBinaryInputException() {
--- src/main/java/com/github/raboro/logic/propositional/exception/NotEnoughInputValuesException.java
+++ src/main/java/com/github/raboro/logic/propositional/exception/NotEnoughInputValuesException.java
@@ -1,5 +1,9 @@
 package com.github.raboro.logic.propositional.exception;
 
+/**
+ * @author Raboro
+ * @since 1.0-SNAPSHOT
+ */
 public class NotEnoughInputValuesException extends RuntimeException {
 
     public NotEnoughInputValuesException() {
