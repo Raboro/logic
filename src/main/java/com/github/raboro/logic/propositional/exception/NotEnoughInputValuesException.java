229ab78b8407f99a046a180ca6cc443021e5c60a
Raboro <marius.woerfelreal@gmail.com>
2023-04-08 17:05:57 +0200

    simplified NotEnoughInputValuesException by removing Throwable

--- src/main/java/com/github/raboro/logic/propositional/exception/NotEnoughInputValuesException.java
+++ src/main/java/com/github/raboro/logic/propositional/exception/NotEnoughInputValuesException.java
@@ -3,18 +3,14 @@ package com.github.raboro.logic.propositional.exception;
 public class NotEnoughInputValuesException extends RuntimeException{
 
     public NotEnoughInputValuesException() {
-        super("Not enough input values", new Throwable());
+        this("Not enough input values");
     }
 
     public NotEnoughInputValuesException(String message) {
-        super(message, new Throwable());
+        super(message);
     }
 
     public NotEnoughInputValuesException(int values) {
-        super(String.format("Given values: %s, are not enough, minimum are 2", values), new Throwable());
-    }
-
-    public NotEnoughInputValuesException(String message, Throwable err) {
-        super(message, err);
+        super(String.format("Given values: %s, are not enough, minimum are 2", values));
     }
 }
