0bf4b81d4eb7edf27ea6718c2abcab354c6af5bd
Raboro <marius.woerfelreal@gmail.com>
2023-04-09 14:43:07 +0200

    added tests for Nor

---
+++ src/test/java/com/github/raboro/logic/propositional/symbols/NorTest.java
@@ -0,0 +1,67 @@
+package com.github.raboro.logic.propositional.symbols;
+
+import com.github.raboro.logic.propositional.exception.NotEnoughInputValuesException;
+import org.junit.jupiter.api.Test;
+
+import static com.github.raboro.logic.propositional.symbols.Nor.nor;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Raboro
+ * @since 1.0-SNAPSHOT
+ */
+class NorTest extends SymbolTest {
+
+    NorTest() {
+        mapper = constructMapper();
+    }
+
+    private SymbolTestMapper constructMapper() {
+        Symbol symbol = new Nor(true, true);
+        Symbol[] symbols = new Symbol[]{
+                new Nor(false, false, false),
+                new Nor(false, false, true),
+                new Nor(false, true, false),
+                new Nor(false, true, true),
+                new Nor(true, false, false),
+                new Nor(true, false, true),
+                new Nor(true, true, false),
+                new Nor(true, true, true)
+        };
+        SymbolTestMapper.SymbolTestMapperBuilder builder = new SymbolTestMapper.SymbolTestMapperBuilder(symbol);
+        return builder.setValueExpected(new boolean[]{true, false, false, false, false, false, false, false})
+                .setSymbol(symbol.symbol)
+                .setValueEqualsNeededSymbols(symbols)
+                .setValueEqualsNeededBinaryReference()
+                .setValueEqualsNeededBooleanReference()
+                .build();
+    }
+
+    @Test
+    void testStaticAndTwoInputs() {
+        assertTrue(nor(false, false));
+        assertFalse(nor(true, false));
+        assertFalse(nor(false, true));
+        assertFalse(nor(true, true));
+    }
+
+    @Test
+    void testStaticAndMultipleInputs() {
+        assertFalse(nor(false, true, false, false));
+        assertFalse(nor(false, true, false));
+        assertFalse(nor(true, false, true, true, false, true));
+        assertTrue(nor(false, false, false, false, false, false));
+        assertTrue(nor(false, false, false));
+    }
+
+    @Test
+    void testExceptionThrownIfInvalidInputNoArguments() {
+        assertThrows(NotEnoughInputValuesException.class, Nor::nor);
+    }
+
+    @Test
+    void testExceptionThrownIfInvalidInputOneArguments() {
+        assertThrows(NotEnoughInputValuesException.class, () -> nor(true));
+        assertThrows(NotEnoughInputValuesException.class, () -> nor(false));
+    }
+}
